a917e4940f057d48301a7f16c184a9fe29188f9b
Zenitha <zenitha26@gmail.com>
2026-06-06 18:32:35 +0530

    fix: clean next config file for cloudflare deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-import './patch-fs.js';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  distDir: '.next_custom_build',
 };
 
 export default nextConfig;
